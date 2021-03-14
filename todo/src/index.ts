5eadecd46d0e5efd9db184788f5d05d65d946e0b
Ferenc Dosa-Racz <dosarf@gmail.com>
2021-03-14 18:09:24 +0200

    TS feature: 'type assertion' + added commands
    TypeScript type assertion example:
    
    ```
    let completedTasks = answers["complete"] as number[];
    ```
    
    - inquirer API allows values of any type to be put as choices
    - therefore the selected choices come untyped
    - type assertion tells the compiler that it's an array of numbers
    
    Added commands (bit of fun)
    
    - adding tasks
    - marking tasks complete / incomplete
    - purging complete tasks

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -13,32 +13,100 @@ let collection = new TodoCollection("Feri", todos)
 
 let newId = collection.addTodo("Go for run");
 let todoItem = collection.getTodoById(newId);
+let showCompleted = true;
 
 function displayTodoList() : void {
   console.log(
     `${collection.userName}'s Todo List'`
     + `(${    collection.getItemCounts().incomplete } items to do)`
   );
-  collection.getTodoItems(true).forEach(item => item.printDetails());
+  collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
 
 enum Commands {
+  Add = "Add New Task",
+  Complete = "Complete Task",
+  Toggle = "Show/Hide Completed",
+  Purge = "Purge Completed Tasks",
   Quit = "Quit"
 }
+
+function promptAdd() : void {
+  console.clear();
+  inquirer
+    .prompt({
+      type: "input",
+      name: "add",
+      message: "Enter task:"
+    })
+    .then( answer => {
+        let task = answer["add"];
+        if (task !== "") {
+          collection.addTodo(task);
+        }
+        promptUser();
+    });
+}
+
+function promptComplete() : void {
+  console.clear();
+  inquirer
+    .prompt({
+      type: "checkbox",
+      name: "complete",
+      message: "Mark Tasks Complete",
+      choices: collection.getTodoItems(showCompleted).map(item => ({
+        name: item.task,
+        value: item.id,
+        checked: item.complete
+      }))
+    })
+    .then(answers => {
+      let completedTasks = answers["complete"] as number[];
+      collection.getTodoItems(true).forEach(item =>
+        collection.markComplete(
+          item.id,
+          completedTasks.find(id => id === item.id) != undefined
+        )
+      )
+      promptUser();
+    });
+}
+
 
 function promptUser() : void {
   console.clear();
   displayTodoList();
-  inquirer.prompt({
-    type: "list",
-    name: "command",
-    message: "Choose option",
-    choices: Object.values(Commands)
-  }).then(answers => {
-    if (answers["command"] !== Commands.Quit) {
-      promptUser();
-    }
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commands)
+    })
+    .then(answers => {
+      let command = answers["command"];
+      switch (command) {
+        case Commands.Toggle:
+          showCompleted = !showCompleted;
+          promptUser();
+          break;
+        case Commands.Add:
+          promptAdd();
+          break;
+        case Commands.Complete:
+          if (collection.getItemCounts().incomplete > 0) {
+            promptComplete();
+          } else {
+            promptUser();
+          }
+          break;
+        case Commands.Purge:
+          collection.removeComplete();
+          promptUser();
+          break;
+      }
+    });
 }
 
 promptUser();
